33a4f84b511082cc4a7105da4a6155c56eaa48bf
Rauf Erk <erk.rauf@gmail.com>
2021-10-09 23:06:27 +0300

    finish

---
+++ src/views/EventCreate.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>EVENT CREATE</h1>
+  </div>
+</template>
---
+++ src/views/EventList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="event-list">
+    <h1>EVENT LIST</h1>
+    <template v-if="events.length">
+      <EventCard
+        v-for="event in events"
+        :key="event.id"
+        name="users"
+        :event="event"
+      />
+    </template>
+  </div>
+</template>
+<script>
+import EventCard from '@/components/EventCard.vue'
+import EventServises from '@/services/EventServises.js'
+
+export default {
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: [],
+    }
+  },
+  created() {
+    EventServises.getEvents()
+      .then(({ data }) => {
+        this.events = data
+      })
+      .catch((error) => console.log(error))
+  },
+}
+</script>
+
+<style></style>
